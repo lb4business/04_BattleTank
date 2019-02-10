acdca796eeacd5bf0d6f40554ce62d72e8de79e6
lb4b <lb4business@gmail.com>
2019-02-10 18:11:13 +0500

    Added UI

---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,7 @@
+// Copyright 2019 Leonid Burylov
+
+#include "TankPlayerController.h"
+
+
+
+
---
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -0,0 +1,20 @@
+// Copyright 2019 Leonid Burylov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
