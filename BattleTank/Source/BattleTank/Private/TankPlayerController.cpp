afb8c67d5f90398cb7252723b5c84ac663b286f5
lb4b <lb4business@gmail.com>
2019-02-10 18:23:56 +0500

    Added TankPlayerController and Tank c++ classes

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -2,6 +2,9 @@
 
 #include "TankPlayerController.h"
 
-
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn()); //What tank are we controlling? TANK is a child of PAWN, so we use Cast to get Tank from Pawn
+}
 
 
